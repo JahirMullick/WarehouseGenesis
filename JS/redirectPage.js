7fd933b00fa4c4e4e70d7db14e4ca6a8a6345e83
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-11 16:17:22 +0530

    Switch page using js

---
+++ JS/redirectPage.js
@@ -0,0 +1,52 @@
+if (document.getElementById("1").addEventListener('click', function () {
+    window.location = "/Html/admin dashboard.html";
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("2").addEventListener('click', function () {
+
+    window.location = '/Html/driver.html';
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("3").addEventListener('click', function(){
+
+    window.location = "/Html/vehicle.html";
+
+})) {
+    console.log('Tab is opened');
+} 
+else if (document.getElementById("4").addEventListener('click', function () {
+
+    window.location = "/Html/trip.html";
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("5").addEventListener('click', function () {
+
+    window.location = "/Html/password.html";
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("6").addEventListener('click', function () {
+
+    window.location = "/Html/LoginForm.html";
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("7").addEventListener('click', function () {
+
+    window.location = "/Html/about us.html";
+    // window.open("/Html/about us.html");
+})) {
+    console.log('Tab is opened');
+}
+else if (document.getElementById("8").addEventListener('click', function () {
+
+    window.location = "/Html/help.html";
+})) {
+    console.log('Tab is opened');
+}
+else {
+    console.log('Nothing');
+}
