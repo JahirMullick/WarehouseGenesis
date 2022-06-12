69cc8646a558a145f3283dd58cb55bcc0e37be9e
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-12 23:27:14 +0530

    Switch the page without reload Header and Title bar

--- JS/redirectPage.js
+++ JS/redirectPage.js
@@ -1,52 +1,27 @@
-if (document.getElementById("1").addEventListener('click', function () {
-    window.location = "/Html/admin dashboard.html";
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("2").addEventListener('click', function () {
 
-    window.location = '/Html/driver.html';
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("3").addEventListener('click', function(){
+function aboutusPage() {
+    $('#refresh-section').load('/Html/about us.html');
+};
+function dasboardPage() {
+    $('#refresh-section').load(location.href + " #refresh-section");
+};
+function driverPage() {
+    $('#refresh-section').load('/Html/driver.html');
+};
+function helpPage() {
+    $('#refresh-section').load('/Html/help.html');
+};
 
-    window.location = "/Html/vehicle.html";
-
-})) {
-    console.log('Tab is opened');
-} 
-else if (document.getElementById("4").addEventListener('click', function () {
-
-    window.location = "/Html/trip.html";
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("5").addEventListener('click', function () {
-
-    window.location = "/Html/password.html";
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("6").addEventListener('click', function () {
-
+function loginPage() {
     window.location = "/Html/LoginForm.html";
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("7").addEventListener('click', function () {
+};
 
-    window.location = "/Html/about us.html";
-    // window.open("/Html/about us.html");
-})) {
-    console.log('Tab is opened');
-}
-else if (document.getElementById("8").addEventListener('click', function () {
-
-    window.location = "/Html/help.html";
-})) {
-    console.log('Tab is opened');
-}
-else {
-    console.log('Nothing');
-}
+function passwordPage() {
+    $('#refresh-section').load('/Html/password.html');
+};
+function tripPage() {
+    $('#refresh-section').load('/Html/trip.html');
+};
+function vehiclePage() {
+    $('#refresh-section').load('/Html/vehicle.html');
+};
