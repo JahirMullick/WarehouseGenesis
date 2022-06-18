a693233a174b21dabb700d6dc72cafa59c678c16
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-18 12:35:13 +0530

    Fetch data from database and show on Pop-Up trip page

--- JS/Trip-pop-up.js
+++ JS/Trip-pop-up.js
@@ -1,3 +1,6 @@
+
+// This function call on when vehicle Driver clicked
+
 document.getElementById('button_driver').addEventListener("click", function() {
 	document.querySelector('.popup-section').style.display = "flex";
 });
@@ -6,7 +9,7 @@ document.querySelector('.dclose').addEventListener("click", function() {
 	document.querySelector('.popup-section').style.display = "none";
 });
 
-
+// This function call on when vehicle Button clicked
 
 document.getElementById('button_truck').addEventListener("click", function() {
 	document.querySelector('.truck_popup_section').style.display = "flex";
@@ -15,3 +18,53 @@ document.getElementById('button_truck').addEventListener("click", function() {
 document.querySelector('.tclose').addEventListener("click", function() {
 	document.querySelector('.truck_popup_section').style.display = "none";
 });
+
+
+// data fetch for truck list table
+fetch("http://localhost:3000/driver")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+
+    // onclick="truck_details()"
+    data.forEach((user) => {
+        $("#pop_Up_tr").append(`
+
+            <tr id=${user.id} onclick="truck_details(${user.id})">
+                <td>
+                    ${user.name}
+                </td>
+                <td>
+                    ${user.status}
+                </td>
+            </tr>
+        
+        `)
+        
+    });
+})
+
+
+// data fetch for truck list table
+fetch("http://localhost:3000/vehicles")
+.then((res) => res.json())
+.then((data) => {
+    console.log(data)
+
+    // onclick="truck_details()"
+    data.forEach((user) => {
+        $("#pop_up_tt").append(`
+
+            <tr id=${user.id} onclick="truck_details(${user.id})">
+                <td>
+                    ${user.model}
+                </td>
+                <td>
+                    ${user.status}
+                </td>
+            </tr>
+        
+        `)
+        
+    });
+})
