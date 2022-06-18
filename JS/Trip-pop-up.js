5e1d24cefe8d4ea0089bb5d92cd49b1adbf5d469
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-18 10:59:04 +0530

    Remove comment code

--- JS/Trip-pop-up.js
+++ JS/Trip-pop-up.js
@@ -5,9 +5,8 @@ document.getElementById('button_driver').addEventListener("click", function() {
 document.querySelector('.dclose').addEventListener("click", function() {
 	document.querySelector('.popup-section').style.display = "none";
 });
-// document.querySelector('.button').addEventListener("click", function() {
-// 	window.close();
-// });
+
+
 
 document.getElementById('button_truck').addEventListener("click", function() {
 	document.querySelector('.truck_popup_section').style.display = "flex";
@@ -16,6 +15,3 @@ document.getElementById('button_truck').addEventListener("click", function() {
 document.querySelector('.tclose').addEventListener("click", function() {
 	document.querySelector('.truck_popup_section').style.display = "none";
 });
-// document.querySelector('.button').addEventListener("click", function() {
-// 	window.close();
-// });
