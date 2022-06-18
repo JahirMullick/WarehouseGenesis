4e2aee6d1c568ff2e8a16a62a9e496f1f385871e
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-18 10:54:07 +0530

    Add Trip pop up window

--- JS/Trip-pop-up.js
+++ JS/Trip-pop-up.js
@@ -1,9 +1,20 @@
-document.getElementById('button').addEventListener("click", function() {
+document.getElementById('button_driver').addEventListener("click", function() {
 	document.querySelector('.popup-section').style.display = "flex";
 });
 
-document.querySelector('.close').addEventListener("click", function() {
+document.querySelector('.dclose').addEventListener("click", function() {
 	document.querySelector('.popup-section').style.display = "none";
+});
+// document.querySelector('.button').addEventListener("click", function() {
+// 	window.close();
+// });
+
+document.getElementById('button_truck').addEventListener("click", function() {
+	document.querySelector('.truck_popup_section').style.display = "flex";
+});
+
+document.querySelector('.tclose').addEventListener("click", function() {
+	document.querySelector('.truck_popup_section').style.display = "none";
 });
 // document.querySelector('.button').addEventListener("click", function() {
 // 	window.close();
