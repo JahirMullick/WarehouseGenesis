b62ac380e381ea00a9c18f25bad5647676528732
MD JAHIR MULLICK <jahirmj2001@gmail.com>
2022-06-19 12:06:04 +0530

    Fetch only active Driver and Available vehicle data on pop-up

--- JS/Trip-pop-up.js
+++ JS/Trip-pop-up.js
@@ -1,34 +1,33 @@
 
 // This function call on when vehicle Driver clicked
 
-document.getElementById('button_driver').addEventListener("click", function() {
-	document.querySelector('.popup-section').style.display = "flex";
+document.getElementById('button_driver').addEventListener("click", function () {
+    document.querySelector('.popup-section').style.display = "flex";
 });
 
-document.querySelector('.dclose').addEventListener("click", function() {
-	document.querySelector('.popup-section').style.display = "none";
+document.querySelector('.dclose').addEventListener("click", function () {
+    document.querySelector('.popup-section').style.display = "none";
 });
 
 // This function call on when vehicle Button clicked
 
-document.getElementById('button_truck').addEventListener("click", function() {
-	document.querySelector('.truck_popup_section').style.display = "flex";
+document.getElementById('button_truck').addEventListener("click", function () {
+    document.querySelector('.truck_popup_section').style.display = "flex";
 });
 
-document.querySelector('.tclose').addEventListener("click", function() {
-	document.querySelector('.truck_popup_section').style.display = "none";
+document.querySelector('.tclose').addEventListener("click", function () {
+    document.querySelector('.truck_popup_section').style.display = "none";
 });
 
 
 // data fetch for truck list table
-fetch("http://localhost:3000/driver")
-.then((res) => res.json())
-.then((data) => {
-    console.log(data)
+fetch("http://localhost:3000/driver?status=active")
+    .then((res) => res.json())
+    .then((data) => {
 
-    // onclick="truck_details()"
-    data.forEach((user) => {
-        $("#pop_Up_tr").append(`
+        // onclick truck_details
+        data.forEach((user) => {
+            $("#pop_Up_tr").append(`
 
             <tr id=${user.id} onclick="truck_details(${user.id})">
                 <td>
@@ -40,20 +39,33 @@ fetch("http://localhost:3000/driver")
             </tr>
         
         `)
-        
-    });
-})
+
+        });
+    })
+
+function tdriver_details(td) {
+    fetch("http://localhost:3000/driver/" + td)
+        .then((res) => res.json())
+        .then((tddata) => {
+
+            let json = tddata;
+
+            let driver_name = json.name;
+
+            document.getElementById("adriver").value = driver_name;
+        });
+}
+
 
 
 // data fetch for truck list table
-fetch("http://localhost:3000/vehicles")
-.then((res) => res.json())
-.then((data) => {
-    console.log(data)
+fetch("http://localhost:3000/vehicles?status=Available")
+    .then((res) => res.json())
+    .then((data) => {
 
-    // onclick="truck_details()"
-    data.forEach((user) => {
-        $("#pop_up_tt").append(`
+        // onclick truck_details()
+        data.forEach((user) => {
+            $("#pop_up_tt").append(`
 
             <tr id=${user.id} onclick="truck_details(${user.id})">
                 <td>
@@ -65,6 +77,17 @@ fetch("http://localhost:3000/vehicles")
             </tr>
         
         `)
-        
-    });
-})
+
+        });
+    })
+
+
+function tvehicles_details(tv) {
+    fetch("http://localhost:3000/vehicles/" + tv)
+        .then((res) => res.json())
+        .then((tvdata) => {
+            let json = tvdata;
+            let truck_model = json.model;
+            document.getElementById("avehicle").value = truck_model;
+        });
+}
